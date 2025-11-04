24fb3fd38f60d4195edc7032aa602bd94b01b7bd
Aftab.Patwekar <Aftab.Patwekar@SMT-LAP-261.smarteinc.local>
2025-11-04 21:12:33 +0530

    cors issues

--- server.js
+++ server.js
@@ -9,9 +9,19 @@ import dotenv from "dotenv";
 
 const app = express();
 dotenv.config();
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://mcp-ui-nine.vercel.app", // ✅ your Vercel frontend
+];
 app.use(
   cors({
-    origin: "http://localhost:5173", // your Vite app
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type"],
   })
